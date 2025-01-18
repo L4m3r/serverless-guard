0d61df70f22f9c7c6e6a50d22748f6e95c0c639e
l4m3r <klimov.lamer@gmail.com>
2025-01-18 22:53:54 +0300

    Add logger

--- app/tg_handler.go
+++ app/tg_handler.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/l4m3r/serverless-guard/app/apigw"
@@ -13,21 +14,25 @@ import (
 )
 
 type app struct {
-	// logger *slog.Logger
-	Bot *tgbotapi.BotAPI
+	logger *slog.Logger
+	Bot    *tgbotapi.BotAPI
 }
 
 func NewApp() (*app, error) {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	botAPIToken := os.Getenv("TG_TOKEN")
 	if botAPIToken == "" {
+		logger.Error("TG_TOKEN not found or empty")
 		return nil, fmt.Errorf("No token")
 	}
 	bot, err := tgbotapi.NewBotAPI(botAPIToken)
 	if err != nil {
+		logger.Error("Cant init bot", "error", err)
 		return nil, err
 	}
 	return &app{
-		Bot: bot,
+		logger: logger,
+		Bot:    bot,
 	}, nil
 }
 
@@ -40,6 +45,7 @@ func TgHandler(ctx context.Context, req *apigw.APIGatewayRequest) (*apigw.APIGat
 	if req.IsBase64Encoded {
 		bodyBytes, err = base64.StdEncoding.DecodeString(string(req.Body))
 		if err != nil {
+			app.logger.Error("Cant decode base64 string", "error", err)
 			return nil, err
 		}
 	} else {
@@ -48,14 +54,17 @@ func TgHandler(ctx context.Context, req *apigw.APIGatewayRequest) (*apigw.APIGat
 	var body = apigw.Request{}
 	err = json.Unmarshal(bodyBytes, &body)
 	if err != nil {
+		app.logger.Error("Unmarshal error", "error", err)
 		return nil, err
 	}
 	message := body.Message
 	reply := tgbotapi.NewMessage(message.Chat.ID, message.Text)
 	_, err = app.Bot.Send(reply)
 	if err != nil {
+		app.logger.Error("Error in bot sending", "error", err)
 		return nil, err
 	}
+	app.logger.Info("Successfuly sent message", "chat_id", message.Chat.ID, "text", message.Text)
 	return &apigw.APIGatewayResponse{
 		StatusCode: 200,
 	}, nil
